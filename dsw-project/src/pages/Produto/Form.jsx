67f81502df06010ee004e32dfaf26d3711bc61e4
Augusto-Rustick <a.rustick@outlook.com>
2023-05-19 15:15:32 -0300

    insert no DAO de produtos

--- dsw-project/src/pages/Produto/Form.jsx
+++ dsw-project/src/pages/Produto/Form.jsx
@@ -1,17 +1,54 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import "./Form.css";
 import { Row, Col, InputGroup } from "react-bootstrap";
 import useForm from "../../hooks/useForm";
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
 const NewProduct = () => {
   const [uploadedImages, setUploadedImages] = useState([]);
   const [autoFormat, setAutoFormat] = useState(false);
   const [gender, setGender] = useState("");
+  const navigate = useNavigate();
 
   const callbackSubmit = async () => {
-    console.log(data);
+    const tamanho = ['PP', '_P', '_M', '_G', 'GG'];
+
+    if(!data.gender){
+      alert('Escolha o Tipo antes de continuar')
+      return
+    }
+
+    const url = 'http://localhost:8080/produto/insert';
+
+    const insertProduto = async (data) => {
+      try {
+        const response = await axios.post(url, data);
+        console.log(response)
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    tamanho.forEach((tamanhoItem) => {
+      const cor = data.corProduto.toUpperCase()
+      const codigo = `${tamanhoItem}${data.gender}${cor}`;
+
+      const postData = {
+        nome: data.nomeProduto,
+        tamanho: tamanhoItem,
+        preco: data.precoProduto,
+        codigo: codigo,
+        tipo: data.gender,
+        descricao: data.descricaoProduto
+      };
+
+      insertProduto(postData);
+    })
+
+    navigate("/home");
   };
 
   const initialState = {
@@ -39,7 +76,7 @@ const NewProduct = () => {
   const handleGenderChange = (event) => {
     const selectedGender = event.target.value;
     setGender(selectedGender);
-    handleChange(event); // Atualizar o estado principal
+    handleChange(event);
   };
 
   const [
@@ -128,15 +165,16 @@ const NewProduct = () => {
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="productGender">
+          <Form.Label>Tipo:</Form.Label>
             <Form.Control
               defaultValue={data.gender}
               as="select"
               onChange={handleGenderChange}
               name="gender"
             >
-              <option value="">Selecione o gênero</option>
-              <option value="masculino">Masculino</option>
-              <option value="feminino">Feminino</option>
+              <option value="">Selecione o tipo</option>
+              <option value="M">Masculino</option>
+              <option value="F">Feminino</option>
             </Form.Control>
           </Form.Group>
 
